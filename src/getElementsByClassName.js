2c26a282cc7a0182de479ceebfe5f82eeff92397
rndmize <rndmize@gmail.com>
2014-02-21 08:02:49 -0800

    Completed stringifyJSON and getElementByClassName
    Also had to fix some issues with the names of the testing variables for
    stringify; the last test for both projects seems to give errors with null
    for some reason, not sure why.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,23 @@
 
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
+  var body = document.body;
+  return traverseTree(className, body);
 };
+
+var traverseTree = function (className, currentNode) {
+  var targets = [];
+
+  for (var i = 0; i < currentNode.classList.length; i++) {
+    if (currentNode.classList[i] === className)
+      targets[i] = currentNode.nodeName;
+  }
+
+  if (currentNode.firstChild.nodeType === 1)
+    targets = targets.concat(traverseTree(className, currentNode.firstChild));
+
+  if (currentNode.nextSibling.nodeType === 1)
+    targets = targets.concat(traverseTree(className, currentNode.nextSibling));
+
+  return targets;
+}
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,72 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+  var str = "";
+  if (typeof obj === "function") return;
+  if (obj instanceof Array) {
+    str += "[";
+  	for (var i = 0; i < obj.length; i++) {
+      if (typeof obj[i] === "function") { // remove functions
+        obj.shift();
+        i--;
+        continue;
+      }
+      if (i !== 0) str += ",";
+  	  if (typeof obj[i] === "object") {
+        str += stringifyJSON(obj[i]);
+      }
+      else {
+        if (typeof obj[i] === "string") str += '"'; // wrap strings in quotes
+        str += obj[i].toString(); 
+        if (typeof obj[i] === "string") str += '"'; // close quotes
+      }
+  	}
+    str += "]";
+  }
+  // "typeof null" returns object, so this case needs to be handled before I can deal with objects
+  else if (obj === null) str += "null";
+  else if (typeof obj === "object") {
+    str += "{";
+    var i = 0;
+
+    for (var item in obj) {
+      if ((typeof obj[item] === "function") || (typeof obj[item] === "undefined")) { // remove functions
+        continue;
+      }
+      console.log(typeof obj[item]);
+      if (i !== 0) str += ",";
+      i++;
+
+      str += '"' + item.toString() + '"' + ':';
+      if ((typeof obj[item] === "object") && (obj[item] !== null)) {
+        str += stringifyJSON(obj[item]);
+      }
+      else {
+        
+        if (typeof obj[item] === "string") str += '"'; // wrap strings in quotes
+        if (obj[item] === null) {
+          str += "null";
+          continue;
+        }
+        str += obj[item].toString();
+        if (typeof obj[item] === "string") str += '"'; // close quotes
+      }
+    }
+    str += "}";
+  }
+  else {
+    if (typeof obj === "string") str += '"'; // wrap strings in quotes
+    str += obj.toString();
+    if (typeof obj === "string") str += '"'; // close quotes
+  }
+  
+    // if key/value pair, convert to string
+    // if object or array, return self
+  // return string
+  return str;
 };
+
+var stringifyJSON2 = function (obj) {
+  // if key/value pair, convert to string
+  // 	
+}
